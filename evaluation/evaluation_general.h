f69bbe1e6d79f9b9d4208d28d3dcfeae67723f01
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-23 15:51:48 -0400

    Documented evaluation_general.h.

--- evaluation/evaluation_general.h
+++ evaluation/evaluation_general.h
@@ -1,6 +1,14 @@
+/**
+ * @file evaluation_general.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief A common header to include everything related to evaluation.
+ * @copyright (c) Smith School of Business, 2023
+ */
+
 #pragma once
 
 #include "evaluator.h"
 #include "estimator.h"
 #include "analyzer.h"
 #include "launcher.h"
+#include "statistics.h"
