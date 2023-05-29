fe8fca27a4f067e4d274481d5ac2e318527d6b23
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-29 13:06:00 -0400

    Documented gm_types.h

--- backend/lagrangian_relaxation/gm_sea/gm_src/gm_types.h
+++ backend/lagrangian_relaxation/gm_sea/gm_src/gm_types.h
@@ -1,20 +1,35 @@
+/**
+ * @file gm_types.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Defines commonly used during gradient method implementation enumerations and types.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 
 namespace gm {
 
+/**
+ * @brief Enumerates implemented Gradient Optimization methods.
+ */
 enum class GradientFamily {
-    UFGM,
-    GM,
-    UPGM
+    UFGM,   ///< Universal Fast Gradient Method
+    GM,     ///< Simple Canonical Gradient Method.
+    UPGM    ///< Universal Primal Gradient Method.
 };
 
+/**
+ * @brief Enumerates prox-functions. 
+ */
 enum class GmProxType {
-    L2SQUARED
+    L2SQUARED   ///< Prox-function in the form of squared L2 distance.
 };
 
+/**
+ * @brief Enumerates regularizers.
+ */
 enum class GmRegularizerType {
-    L2SQUARED
+    L2SQUARED   ///< Regularizer in the form of squared L2 distance.
 };
 
 
