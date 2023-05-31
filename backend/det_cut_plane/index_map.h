e4c8d1f1fd20a0455acf26759bca1967ef4def1b
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-31 18:41:13 -0400

    Documented deterministic cutting plane.

--- backend/det_cut_plane/index_map.h
+++ backend/det_cut_plane/index_map.h
@@ -1,3 +1,9 @@
+/**
+ * @file index_map.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Declares DCP Index Map - manager for indices used in Deterministic Cutting Plane method.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 #include "../ipopt/index_map.h"
@@ -8,15 +14,39 @@
 namespace sea {
 namespace backend {
 
+/**
+ * @brief Index map. Uses the same indexation model like IpoptIndexMap, but in addition
+ * adds entries specific for Det-Cut-Plane allotment.
+ */
 struct DcpIndexMap: IpoptIndexMap {
+    /// @brief Maps time-itinerary to Zi index.
     std::vector<std::vector<unsigned>> timeItineraryToZi;
-    unsigned demandZCount = 0; // First <demandZCount> variables from variableCount are z_i
+    /// @brief First <demandZCount> variables from variableCount are z_i
+    unsigned demandZCount = 0;
 };
 
+/**
+ * @brief Initialization for DcpIndexMap.
+ * 
+ * @param[in] input InputData to use for initialization.
+ * @param[out] indexMap IndexMap to initialize.
+ */
 void dcpCreateIndexMap(const InputData& input, DcpIndexMap& indexMap);
 } // namespace backend
 
+/**
+ * @brief Exports DCP Index Map to file.
+ * 
+ * @param filePath Export path.
+ * @param indexMap Entity to export.
+ */
 void writeToFile(const std::string& filePath, const backend::DcpIndexMap& indexMap);
+/**
+ * @brief Reads DCP Index Map from file.
+ * 
+ * @param[out] filePath Path to read from.
+ * @param[in] indexMap Entity to import.
+ */
 void loadFromFile(const std::string& filePath, backend::DcpIndexMap* indexMap);
 
 } // namespace sea
--- backend/det_cut_plane/index_map.hpp
+++ backend/det_cut_plane/index_map.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file index_map.hpp
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Template methods for DCP Index Map.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 #include "index_map.h"
@@ -14,6 +20,13 @@
 
 namespace cereal {
 
+/**
+ * @brief Template serialization for DcpIndexMap.
+ * 
+ * @tparam Archive Type of archive.
+ * @param[out] archive The serialization archvie.
+ * @param[out] indexMap The object to process.
+ */
 template<class Archive>
 void serialize(Archive& archive, sea::backend::DcpIndexMap& indexMap) {
     archive(cereal::base_class<sea::backend::IpoptIndexMap>(&indexMap),
