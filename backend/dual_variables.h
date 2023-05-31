fee33469a67e7d95fc851d2c8f5dc8f3e243d313
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-31 16:10:20 -0400

    Documented dual_variables.h.

--- backend/dual_variables.h
+++ backend/dual_variables.h
@@ -1,3 +1,9 @@
+/**
+ * @file dual_variables.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Implements Dual Variables structure: template and specification for double type.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 #include <vector>
@@ -8,12 +14,22 @@ namespace sea {
 using std::vector;
 using std::unordered_map;
 
+/**
+ * @brief Template for dual variables and related structures. 
+ * Contains two components: component that corresponds to lambda variables, and component that
+ * corresponds to mu variables.
+ * 
+ * @tparam Type The type of inner values. Options: CppAD<double>, double, bool.
+ */
 template<class Type>
 struct DualTemplate {
-    vector<Type> lambdaVariables; // size = # of related arcs
-    vector<Type> muVariables;     // size = # of itineraries
+    /// @brief Components that correspond to lambda duals. The size = # of related arcs.
+    vector<Type> lambdaVariables; 
+    /// @brief Components that correspond to mu duals. The size = # of itineraries.
+    vector<Type> muVariables;
 };
 
+/// @brief Particular case - template with double. Represents commonly used dual variables.
 using DualVariables = DualTemplate<double>;
 
 } // namespace sea
