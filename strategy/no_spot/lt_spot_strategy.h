bd10f05a03d2d3773ed4c361691ad919f845ef25
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-31 21:43:18 -0400

    Documented no-spot-aware long-term only wrappers.

--- strategy/no_spot/lt_spot_strategy.h
+++ strategy/no_spot/lt_spot_strategy.h
@@ -1,3 +1,9 @@
+/**
+ * @file lt_spot_strategy.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Template wrapper around Spot Market Strategy for no-spot (long-term only) setting.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 #include "../../backend/ipopt/ipopt_backend.h"
@@ -11,28 +17,54 @@
 namespace sea {
 namespace strategy {
 
+/**
+ * @brief Template Spot Market Strategy which works without spot market.
+ * In some sense, long-term spot market strategy. In the language of design patterns, implements
+ * a Wrapper.
+ * 
+ * @tparam Strategy Some Spot Market Strategy, that supports no-spot regime.
+ */
 template<typename Strategy>
 class LTSpotMarketStrategy : public ISpotMarketStrategy {
-
 public:
+    /**
+     * @brief Constructor for Long-Trem Spot Market Strategy.
+     * 
+     * @param aConfig Configuration for inner strategy object.
+     */
     LTSpotMarketStrategy(const typename Strategy::ConfigType& aConfig)
         : strategy(new Strategy(aConfig)){
         reset();
     }
-
+    /**
+     * @brief Supplies inner strategy with allotment decision.
+     * 
+     * @param decisionManager Decision Manager for informing about allotment decision.
+     */
     virtual void supplyAllotmentDecision(const DecisionManagerPtr& decisionManager) override {
         strategy->supplyAllotmentDecision(decisionManager);
         strategy->resetLastUpdate();
     }
-
+    /**
+     * @brief Retrieves utilization from the wrapped strategy.
+     * 
+     * @return The maximal allowed capacity utilization ratio.
+     */
     virtual double getUtilizationRatio() {
         return strategy->getUtilizationRatio();
     }
-
+    /**
+     * @brief Builds a name for the wrapped strategy.
+     * 
+     * @return The strategy name.
+     */
     virtual std::string getName() override {
         return "LT_modification_" + strategy->getName();
     }
-
+    /**
+     * @brief Soft-reset. Calls soft-reset of the wrapped strategy, and forces 
+     * backends to ignore spot market.
+     */
     virtual void reset() override {
         strategy->reset();
         auto backends = strategy->getBackends();
@@ -44,45 +76,83 @@ public:
         }
         strategy->resetLastUpdate();
     }
-
+    /**
+     * @brief Hard-resets the wrapped object, and calls soft reset from wrapper afterwards.
+     */
     virtual void hardReset() override {
         strategy->hardReset();
         reset();
     }
-
+    /**
+     * @brief Delegates submitAction to the wrapped object.
+     * 
+     * @param action The Manager with Action.
+     */
     virtual void submitAction(ConstActionManagerPtr action) override {
         strategy->submitAction(action);
     }
+    /**
+     * @brief Delegates makeDecision to the wrapped strategy.
+     * 
+     * @return Manager with decision.
+     */
     virtual ConstDecisionManagerPtr makeDecision() override {
         return strategy->makeDecision();
     }
-
+    /**
+     * @brief Whether warm backends are expected and supported.
+     * 
+     * @return If warm backends are expected and supported, returns true. Returns false otherwise.
+     */
     virtual bool needWarmBackends()  {
         return false;
     }
-    virtual void setBackends(const BackendHolder& /*holder*/) {
+    /**
+     * @brief Set backends method is not allowed, since managed by the wrapper.
+     * Therefore, throws logic_error.
+     * 
+     * @param holder The holder with backends.
+     */
+    virtual void setBackends(const BackendHolder& holder) {
         throw std::logic_error("setBackends is not allowed for LTSpotMarketStrategy");
     }
-
+    /**
+     * @brief Gets the evaluation-optimization history. Delegates to the wrapped object.
+     * 
+     * @return Map from measurement name to a sequence of measured values.
+     */
     virtual std::map<std::string, std::vector<double>> getStory() const override {
         return strategy->getStory();
     }
-
+    /**
+     * @brief Delegates resetLastUpdate to the wrapped object.
+     */
     virtual void resetLastUpdate() {
         strategy->resetLastUpdate();
     }
+    /**
+     * @brief Checks whether history is tracked. Delegates to the wrapped object.
+     * 
+     * @return If history is tracked, returns true. Returns false otherwise.
+     */
     virtual bool isStoryTracked() const override {
         return strategy->isStoryTracked();
     }
-
+    /**
+     * @brief Virtual Destructor to ensure correct C++ polymorphism.
+     */
     virtual ~LTSpotMarketStrategy() {};
-
+    /**
+     * @brief Getter for the backends. Since backends are managed by the wrapper. this 
+     * method throws logic_error.
+     * 
+     * @return Abstract class required to return Holder with Backends.
+     */
     virtual BackendHolder getBackends() override {
         throw std::logic_error("getBackends is not allowed for LTSpotMarketStrategy");
     }
-
-
 private:
+    /// @brief Wrapped strategy object.
     std::unique_ptr<ISpotMarketStrategy> strategy;
 };
 
--- strategy/no_spot/nospot_aware.h
+++ strategy/no_spot/nospot_aware.h
@@ -1,3 +1,9 @@
+/**
+ * @file nospot_aware.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Template implementation for long-term only nospot-aware allotment strategy.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 #include "../abstract_allotment_strategy.h"
@@ -5,23 +11,46 @@
 namespace sea {
 namespace strategy {
 
+/**
+ * @brief Wrapper around allotment strategy to enforce no-spot setting (long-term only).
+ * 
+ * @tparam LongStrategy Type of the wrapped allotment strategy.
+ */
 template<typename LongStrategy>
 class NospotAwareAllotmentStrategy : public IAllotmentStrategy {
 public:
+    /**
+     * @brief Constructor for no-spot-aware long-term only allotment strategy.
+     * 
+     * @param aConfig Configuration for the wrapped strategy.
+     */
     NospotAwareAllotmentStrategy(
             const typename LongStrategy::ConfigType& aConfig) :
                 strategy(new LongStrategy(aConfig)) {
             reset();
     }
-
+    /**
+     * @brief Set the maximal allowed capacity Utilization Ratio. Delegates to the wrapped
+     * object.
+     * 
+     * @param ratio The maximal allowed capacity utilization ratio.
+     */
     virtual void setUtilizationRatio(double ratio) override {
         strategy->setUtilizationRatio(ratio);
     };
-
+    /**
+     * @brief Gets the maximal allowed capacity Utilization Ratio. Delegates to the
+     * wrapped object.
+     * 
+     * @return The maximal allowed capacity utilization ratio.
+     */
     virtual double getUtilizationRatio() override {
         return strategy->getUtilizationRatio();
     }
-
+    /**
+     * @brief Soft-reset. Delegates soft-reset, and ensures that backends are 
+     * set to the no-spot (long-term only) mode.
+     */
     virtual void reset() override {
         strategy->reset();
         auto backends = strategy->getBackends();
@@ -36,30 +65,52 @@ public:
         // Nospot-scenario is not supported in DCP.
         assert(backends.dcpBackend == nullptr);
     }
-
+    /**
+     * @brief Hard reset. Delegates to the wrapped strategy and calls soft-reset.
+     */
     virtual void hardReset() override {
         strategy->hardReset();
         reset();
     }
-
+    /**
+     * @brief Method to make allotment decision. Delegates to the wrapped strategy.
+     * 
+     * @return Decision Manager with allotment decision and other relevant fields.
+     */
     virtual DecisionManagerPtr provideAllotments() override {
         return strategy->provideAllotments();
     }
-
+    /**
+     * @brief Method to get backends. Delegates to the wrapped object.
+     * 
+     * @return Holder with backends.
+     */
     virtual BackendHolder getBackends() override {
         return strategy->getBackends();
     }
-
+    /**
+     * @brief Creates a name for long-term no-spot-aware allotment strategy.
+     * 
+     * @return The created name for the wrapper.
+     */
     virtual std::string getName() override {
         return strategy->getName() + "_nospot_aware";
     }
+    /**
+     * @brief Delegates value estimation to the wrapped strategy.
+     * 
+     * @return The estimated profit.
+     */
     virtual double getValueEstimation() override {
         return strategy->getValueEstimation();
     }
-
+    /**
+     * @brief Virtual destructor to ensure correct C++ polymorphism.
+     */
     virtual ~NospotAwareAllotmentStrategy() {};
 
 private:
+    /// @brief The wrapped allotment strategy.
     std::unique_ptr<IAllotmentStrategy> strategy;
 };
 
