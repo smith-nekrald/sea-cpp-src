f3095a1598519f8095c833f2c204c4717620a2cc
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-31 20:54:55 -0400

    Documented Ipopt spot market and allotment strategies.

--- strategy/ipopt/ipopt_allotment_strategy.h
+++ strategy/ipopt/ipopt_allotment_strategy.h
@@ -1,3 +1,9 @@
+/**
+ * @file ipopt_allotment_strategy.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Declaration and relevant structures for Ipopt Allotment Strategy.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 #include "../abstract_allotment_strategy.h"
@@ -9,29 +15,45 @@
 namespace sea {
 namespace strategy {
 
-struct IpoptInnerConfig {
+/**
+ * @brief Strategy-specific inner configuration.
+ */
+struct IpoptInnerConfig {};
 
-};
-
+/**
+ * @brief Configuration for IpoptAllotment Strategy, contains parent configuration and 
+ * strategy-specific configuration.
+ */
 struct IpoptAllotmentStrategyConfig {
+    /// @brief Configures abstract allotment strategy, parent class.
     AllotmentStrategyConfig abstractConfig;
+    /// @brief Holder with backend configurations.
     BackendConfigHolder backendConfigs;
-    vector<bool> forcedAllotments;
+    /// @brief If true, forced allotments are fixed, and other relevant fields are computed.
     bool useForcedAllotments = false;
-
+    /// @brief The vector with forced allotments.
+    vector<bool> forcedAllotments;
+    /// @brief Strategy-specific inner configuration.
     IpoptInnerConfig innerConfig;
 };
 
+/**
+ * @brief Ipopt Allotment Strategy is a method to make allotments based on fluid approximation.
+ */
 class IpoptAllotmentStrategy: public AbstractAllotmentStrategy {
 public:
+    /// @brief Short naming for configuration type.
     using ConfigType=IpoptAllotmentStrategyConfig;
 
 public:
+    /**
+     * @brief Constructs Ipopt Allotment Strategy.
+     * 
+     * @param aConfig Configuration for Ipopt Allotment Strategy.
+     */
     IpoptAllotmentStrategy(const IpoptAllotmentStrategyConfig& aConfig)
             : AbstractAllotmentStrategy(
-                    aConfig.abstractConfig,
-                    aConfig.backendConfigs,
-                    "ipopt_allotment")
+                aConfig.abstractConfig, aConfig.backendConfigs, "ipopt_allotment")
             , innerConfig(aConfig.innerConfig) {
         reset();
         assert(config.type == AllotmentStrategyType::IPOPT);
@@ -43,20 +65,47 @@ public:
             memorizedSelection = forcedAllotments;
         }
     }
+    /**
+     * @brief Soft reset. Resets inner state without resetting backends.
+     */
     void reset() override;
+    /**
+     * @brief Hard reset. Also resets backends. However, remembers 
+     * memorizedSelection for warm-start.
+     */
     void hardReset() override;
-    // Creates new DecisionManagerPtr and fills field related to selected allotments.
+    /**
+     * @brief Makes allotment decision based on fluid approximation.
+     * Creates new DecisionManagerPtr and fills field related to selected allotments.
+     * 
+     * @return DecisionManagerPtr 
+     */
     virtual DecisionManagerPtr provideAllotments() override;
+    /**
+     * @brief Virtual destructor for effective C++ polymorphism.
+     */
     virtual ~IpoptAllotmentStrategy() {};
 
 private:
+    /**
+     * @brief Logs the selected allotments.
+     * 
+     * @param selection The selected allotments.
+     */
     void logAllotmentSelection(const vector<bool>& selection) const;
+    /**
+     * @brief Logs the updated decision.
+     * 
+     * @param result The Manager with updated Decision to update.
+     */
     void logUpdatedDecision(ConstDecisionManagerPtr result) const;
 
 private:
+    /// @brief Allotment selection for warm start.
     vector<bool> memorizedSelection;
+    /// @brief Enforced allotment selection.
     vector<bool> forcedAllotments;
-
+    /// @brief Algorithm-specific configuration.
     IpoptInnerConfig innerConfig;
 };
 
--- strategy/ipopt/ipopt_spot_strategy.h
+++ strategy/ipopt/ipopt_spot_strategy.h
@@ -1,3 +1,9 @@
+/**
+ * @file ipopt_spot_strategy.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Declares Ipopt Spot Market Strategy, relevant structures and API.
+ * @copyright (c) Smith School of Business, 2023
+ */
 #pragma once
 
 #include "../../backend/ipopt/ipopt_backend.h"
@@ -10,33 +16,75 @@
 namespace sea {
 namespace strategy {
 
+/**
+ * @brief Configuration for Ipopt Spot Market Strategy.
+ */
 struct IpoptSpotMarketStrategyConfig {
+    /// @brief Configuration component for Spot Market Strategy.
     SpotMarketStrategyConfig abstractConfig;
+    /// @brief Holder with backend configuration.
     BackendConfigHolder backendConfigs;
 };
 
+/**
+ * @brief Ipopt Spot Market Strategy - strategy to make spot-market decisions based on 
+ * fluid approximation approach.
+ */
 class IpoptSpotMarketStrategy : public AbstractSpotMarketStrategy {
 public:
+    /// @brief Simple naming for configuration structure.
     using ConfigType=IpoptSpotMarketStrategyConfig;
 
 public:
+    /**
+     * @brief Constructor for a new Ipopt Spot Market Strategy.
+     * 
+     * @param aConfig Configuration for Ipopt Spot Market Strategy.
+     */
     IpoptSpotMarketStrategy(const IpoptSpotMarketStrategyConfig& aConfig)
         : AbstractSpotMarketStrategy(
-                aConfig.abstractConfig,
-                aConfig.backendConfigs,
-                "ipopt_spot") {
+                aConfig.abstractConfig, aConfig.backendConfigs, "ipopt_spot") {
         reset();
     }
+    /**
+     * @brief Soft reset. Resets inner state without resetting backends.
+     */
     void reset() override;
+    /**
+     * @brief Recomputes parameters, if relevant.
+     * 
+     * @param event Current event on the evaluation trajectory.
+     */
     void updateParams(const InputData::Event& event) override;
+    /**
+     * @brief Provides strategy with backends.
+     * 
+     * @param holder Holder with backends.
+     */
     void setBackends(const BackendHolder& holder) override;
+    /**
+     * @brief Virtual destructor for correct C++ polymorphism.
+     */
     virtual ~IpoptSpotMarketStrategy() {};
 
 protected:
+    /**
+     * @brief Strategy-speficific implementation for processing cut-off events.
+     * 
+     * @param event The current cut-off event to process.
+     */
     virtual void processCutoff(const InputData::Event& event) override;
 
 private:
+    /**
+     * @brief Logs that updateParams is started.
+     * 
+     * @param needUpdate If update is needed.
+     */
     void logEnteredUpdateParams(bool needUpdate) const;
+    /**
+     * @brief Logs that updateParams is over.
+     */
     void logLeftUpdateParams() const;
 };
 
