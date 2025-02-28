3b660e29ab742edea5f5d232491fc409e2fd6747
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2025-02-28 17:50:34 -0500

    Updated local optimal price computation.

--- algorithm/greedy_algorithm.cpp
+++ algorithm/greedy_algorithm.cpp
@@ -281,36 +281,56 @@ void GreedyAlgorithm::processPricing(const InputData::Event& event) {
         const InputData::Port& finalPort = input.ports[finalNode.portId];
         shippingCost += finalPort.offHiringCost;
 
+
         // Find optimal price and demand.
         double optimalDemand = 0.;
+        double optimalPrice = INF;
         const double EPS = 1e-3;
         if (demand.type == Demand::Type::exponential) {
-            optimalDemand = demand.scale / std::exp(1 + shippingCost * demand.sensitivity);
+            optimalDemand = demand.scale / std::exp(
+                    route.showRate.estimatedProba
+                        * (1 + shippingCost * demand.sensitivity)
+                    + (1 - route.showRate.estimatedProba)
+                        * (1 + route.returnPrice * demand.sensitivity));
             optimalDemand = std::min(optimalDemand, maxCapacity);
             const double LOG_EPS = 1e-50;
-            double optimalPrice = 1. / demand.sensitivity * std::log(
+            optimalPrice = 1. / demand.sensitivity * std::log(
                     LOG_EPS + demand.scale / optimalDemand);
-            assert(optimalPrice + EPS >= 0);
-            prices[idx].second = optimalPrice;
             assert(optimalDemand <= demand.scale + EPS);
         } else if (demand.type == Demand::Type::linear) {
             assert(demand.multiplicative < 0.);
-            double optimalPrice = 0.5 * (
-                    shippingCost - demand.additive / demand.multiplicative);
+            optimalPrice = 0.5 * (
+                    shippingCost * route.showRate.estimatedProba
+                    + route.returnPrice * (1 - route.showRate.estimatedProba)
+                    - demand.additive / demand.multiplicative);
             optimalPrice = std::max(optimalPrice,
                     (demand.additive - maxCapacity) / (-demand.multiplicative));
-            optimalPrice = std::min(optimalPrice,
-                    demand.additive / (-demand.multiplicative));
-            assert(optimalPrice >= 0.);
-            prices[idx].second = optimalPrice;
+            optimalPrice = std::min(optimalPrice, demand.additive / (-demand.multiplicative));
             optimalDemand = demand.additive + optimalPrice * demand.multiplicative;
             assert(optimalDemand + EPS >= 0. && optimalDemand <= demand.additive + EPS);
         } else {
             throw std::logic_error("Unexpected demand type!");
+
         }
         // Robustness in prices and demand.
+        optimalPrice += EPS;
         optimalDemand += EPS;
-        prices[idx].second += EPS;
+        assert(optimalPrice >= 0);
+
+        // Revenue-based verification.
+        double expectedRevenue
+            = route.showRate.estimatedProba * optimalDemand
+                * (optimalPrice - shippingCost)
+            + (1 - route.showRate.estimatedProba) * optimalDemand
+                * (optimalPrice - route.returnPrice);
+        if (expectedRevenue + EPS < 0.) {
+            optimalPrice = INF;
+            optimalDemand = EPS;
+        }
+
+        // Setting field values.
+        prices[idx].second = optimalPrice;
+
         // Update greedy stats.
         double expectedAmount = optimalDemand * route.showRate.estimatedProba;
         for (unsigned idxArc : route.orderedArcs) {
