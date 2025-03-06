883b5102de5f2ce1acd51da6f6667a14c7ea3bbe
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2025-03-06 15:43:00 -0500

    Improved CLI interface.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <fstream>
 #include <cassert>
+#include <set>
 #include <filesystem>
 
 #include <jsoncpp/json/json.h>
@@ -23,7 +24,20 @@ namespace json = Json;
  * @param argv The array with CLI arguments.
  * @return The program exit code.
  */
-int main([[maybe_unused]] int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
+    const int N_ARGUMENTS = 2;
+    const std::set<std::string> HELP_ARGUMENTS = {"--help", "-h"};
+    bool helpCalled = false;
+    for (unsigned idx = 1; idx < static_cast<unsigned>(argc); ++idx) {
+        if (HELP_ARGUMENTS.find(std::string(argv[idx])) != HELP_ARGUMENTS.end()) {
+            helpCalled = true;
+            break;
+        }
+    }
+    if (argc != N_ARGUMENTS || helpCalled) {
+        std::cerr << "Usage: " << argv[0] << " <path-to-json-with-config>" << std::endl;
+        return !helpCalled;
+    }
     assert(argc == 2);
     std::string configPath = argv[1];
     std::ifstream configStream(configPath);
