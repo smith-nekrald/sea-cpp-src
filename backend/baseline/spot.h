d02d880f06ae6898ede32892a2799b773ef60662
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2025-03-09 08:16:31 -0400

    Documented baseline backend.

--- backend/baseline/spot.h
+++ backend/baseline/spot.h
@@ -1,3 +1,9 @@
+/**
+ * @file spot.h
+ * @author Aliaksandr Nekrashevich (aliaksandr.nekrashevich@queensu.ca)
+ * @brief Defines spot pricing itinerary sorters and itinerary order metrics.
+ * @copyright (c) Smith School of Business, 2025
+ */
 #pragma once
 
 #include "interface.h"
@@ -166,12 +172,36 @@ public:
      * @param config The baseline spot configuration.
      */
     CompositeSpotSorter(const BaselineSpotConfig& config);
+    /**
+     * @brief Selects the order of itineraries by applying multiple sorters
+     * and selecting the order with the highest metric score.
+     * 
+     * @param event The considered pricing event.
+     * @param stats Current state information.
+     * @return The suggested order of itinerary indices. 
+     */
     virtual std::vector<unsigned> selectOrder(
             const InputData::Event& event, const BaselineStats& stats) const;
+    /**
+     * @brief Getter for the composite spot pricing itinerary sorter name.
+     * 
+     * @return The name of the composite spot pricing itinerary sorter.
+     */
     virtual std::string getName() const;
+    /**
+     * @brief Virtual destructor to ensure correct C++ polymorphism.
+     */
     virtual ~CompositeSpotSorter() {};
 
 private:
+    /**
+     * @brief Logs metric values for ordered vector of itineraries.
+     * 
+     * @param sorterName The name of the sorter providing the order. 
+     * @param spotOrder The order of itineraries.
+     * @param reverseFlag Flag indicating if the order is reversed.
+     * @param score The metric score value for the order.
+     */
     void logScorePerOrder(
             const std::string& sorterName,
             const std::vector<unsigned>& spotOrder,
@@ -179,23 +209,51 @@ private:
             double score) const;
 
 private:
+    /// @brief Input manager.
     ConstInputManagerPtr inputManager;
+    /// @brief Links manager.
     ConstLinksManagerPtr linksManager;
+    /// @brief Spot pricing itinerary order metric.
     std::unique_ptr<ISpotOrderMetric> metric;
+    /// @brief Vector of spot pricing itinerary sorters.
     std::vector<std::unique_ptr<ISpotSorter>> sorters;
 };
 
+/**
+ * @brief Metric for ordered vector of itineraries at spot pricing event.
+ * Proxy of the total expected profit for the event.
+ */
 class SpotEventProfitMetric : public ISpotOrderMetric {
 public:
+    /**
+     * @brief Constructor for SpotEventProfitMetric.
+     * 
+     * @param config The baseline spot configuration.
+     */
     SpotEventProfitMetric(const BaselineSpotConfig& config);
+    /**
+     * @brief Computes the expected profit obtained on first-come first-served basis
+     * when itineraries are considered in the order defined at vector spotOrder and
+     * priced in the locally optimal fashion.
+     * 
+     * @param spotOrder The order of itineraries.
+     * @param event The considered pricing event.
+     * @param stats Current state information.
+     * @return The score value for the ordered vector of itineraries.
+     */
     virtual double score(
             const std::vector<unsigned>& spotOrder,
             const InputData::Event& event,
             const BaselineStats& stats) const override final;
+    /**
+     * @brief Virtual destructor to ensure correct C++ polymorphism.
+     */
     virtual ~SpotEventProfitMetric() {};
 
 private:
+    /// @brief Input manager.
     ConstInputManagerPtr inputManager;
+    /// @brief Links manager.
     ConstLinksManagerPtr linksManager;
 };
 
